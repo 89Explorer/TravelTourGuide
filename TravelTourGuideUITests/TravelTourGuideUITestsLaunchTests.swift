86d15aaf3fedf3d00ea8712a17fe6063c88c293b
89Explorer <leral3@naver.com>
2024-09-02 16:00:10 +0900

    Initial Commit

---
+++ TravelTourGuideUITests/TravelTourGuideUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TravelTourGuideUITestsLaunchTests.swift
+//  TravelTourGuideUITests
+//
+//  Created by 권정근 on 9/2/24.
+//
+
+import XCTest
+
+final class TravelTourGuideUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
